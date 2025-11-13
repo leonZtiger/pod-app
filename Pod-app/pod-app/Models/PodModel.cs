04c0190282df6acb80fe7007dbdd0ebfcaa85c84
leon <leon_bynander@icloud.com>
2025-11-13 12:48:16 +0100

    Pod avsnitt model

---
+++ Pod-app/pod-app/Models/PodModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pod_app.Models
+{
+    internal class PodModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Duration { get; set; }
+        public string URL { get; set; }
+        public string Category { get; set; }
+        public bool isSaved { get; set; }
+    }
+}
