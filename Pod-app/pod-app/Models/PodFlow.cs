dfb1dc379508eb086f513caa3d082300f8c7c782
leon <leon_bynander@icloud.com>
2025-11-13 13:12:38 +0100

    Pod flödde model

---
+++ Pod-app/pod-app/Models/PodFlow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pod_app.Models
+{
+    internal class PodFlow
+    {
+        public List<PodModel> Pods { get; set; }
+    }
+}
