f700d8f5816a72d27b6712572adf3f651454f99d
Gustaf0377 <153506204+Gustaf0377@users.noreply.github.com>
2025-11-26 13:54:22 +0100

    pop up för Namn och kategori

---
+++ pod-app/pod-app/PresentationLayer/Views/PodcastDetailsDialog.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+using MongoDB.Driver;
+using pod_app.DataLayer;
+
+namespace pod_app.PresentationLayer.Views
+{
+    public partial class PodcastDetailsDialog : Window
+    {
+        public PodcastDetailsDialog()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
+
+          
+
+
